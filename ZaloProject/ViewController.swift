7d2005e4279081e83e668f7038b496382f32ddad
TTLGame <ttlgame123@gmail.com>
2021-09-10 21:11:55 +0700

    hi

--- ZaloProject/ViewController.swift
+++ ZaloProject/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         //test
-        
+        //hi
         // Do any additional setup after loading the view.
     }
 
